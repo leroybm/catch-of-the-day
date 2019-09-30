1d962207ff5975c04984984d4fd3be02c0e5539d
Leroy Medeiros <lelo.drax@gmail.com>
2019-09-30 12:02:29 -0300

    feat: adds router

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  storeInput = React.createRef()
+
+  goToStore = (event) => {
+    event.preventDefault()
+    this.props.history.push(`/store/${this.storeInput.current.value}`)
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.storeInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit STore</button>
       </form>
     )
