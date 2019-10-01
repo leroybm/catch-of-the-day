c587753f3b47a8ab463fd1c6e8de3c449da5ec37
Leroy Medeiros <lelo.drax@gmail.com>
2019-10-01 12:04:12 -0300

    feat: adds firebase database

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyAkDilsLrwDFbyEzqk8w0YLJ9eo583utGI',
+  authDomain: 'catch-of-the-day-leroy-29834.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-leroy-29834.firebaseio.com',
+})
+
+const base = Rebase.createClass(firebase.database(firebaseApp))
+
+export { firebaseApp }
+
+export default base
--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,24 @@ import Order from './Order'
 import Inventory from './Inventory'
 import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
+import base from '../base'
 
 class App extends React.Component {
   state = {
     fishes: {},
     orders: {},
+  }
+
+  componentDidMount() {
+    const { params } = this.props.match
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes',
+    })
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref)
   }
 
   createFish = fish => {
